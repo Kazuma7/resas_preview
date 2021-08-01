0285067d510b33a99ec48c24bf6e281488f25dae
Kazuma7 <deetbeet1234@yahoo.co.jp>
2021-08-02 01:50:43 +0900

    feat 関数コンポーネント切り出し

---
+++ src/fetchPrefectures.js
@@ -0,0 +1,18 @@
+const fetchPrefectures = async (
+    apiKey
+) => {
+    const resPref = await fetch('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
+        headers: { 'X-API-KEY': apiKey }
+    })
+    const json = await resPref.json()
+
+    
+
+    
+    
+
+    return json.result;
+    // series: preffInfo
+}
+
+export{fetchPrefectures}
