76df8aa6b35ec148192f1c6c331fde456729d8cd
Kazuma7 <deetbeet1234@yahoo.co.jp>
2021-08-02 11:26:18 +0900

    style コメントの追加

---
+++ src/function/fetchPrefectures.js
@@ -0,0 +1,29 @@
+const fetchPrefectures = async (
+    apiKey
+) => {
+
+    //１ヶ月以上経過したらストレージのデータを削除する
+    if (localStorage.getItem("prefNameDate")) {
+        const date = JSON.parse(localStorage.getItem("prefNameDate"))
+        if((new Date(date)).getMonth() !== (new Date()).getMonth()){
+            localStorage.removeItem("prefName");  
+        }
+    }
+
+    //ローカルストレージにデータがあればそれを使う
+    if (localStorage.getItem("prefName")) {
+        return JSON.parse(localStorage.getItem("prefName"))
+    }
+
+    //ローカルストレージにデータがなければRESAS-APIを通してデータを持ってくる
+    const resPref = await fetch('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
+        headers: { 'X-API-KEY': apiKey }
+    })
+    const json = await resPref.json()
+    localStorage.setItem("prefNameDate", JSON.stringify(new Date()))
+    localStorage.setItem("prefName", JSON.stringify(json.result));
+    return json.result;
+
+}
+
+export{fetchPrefectures}
